5aae32ecd1069c899ad5c7648d465fd2628de763
premtechie <premtechie07@gmail.com>
2020-08-29 00:54:41 +0530

    Updates

---
+++ precache-manifest.1425f3d1d091731a145f762eceb17455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3077c2369e872a38c87c5c936bc02589",
+    "url": "/Matrix-app/index.html"
+  },
+  {
+    "revision": "6364ffa77af69197d642",
+    "url": "/Matrix-app/static/css/main.48054420.chunk.css"
+  },
+  {
+    "revision": "ab0183e6207369f436b4",
+    "url": "/Matrix-app/static/js/2.10ef2198.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Matrix-app/static/js/2.10ef2198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6364ffa77af69197d642",
+    "url": "/Matrix-app/static/js/main.2a85ba3b.chunk.js"
+  },
+  {
+    "revision": "ca57facbc81b7a1c88ae",
+    "url": "/Matrix-app/static/js/runtime-main.74acec06.js"
+  },
+  {
+    "revision": "3e6faefb66a8928a194938479354a258",
+    "url": "/Matrix-app/static/media/robo.3e6faefb.jpg"
+  }
+]);
